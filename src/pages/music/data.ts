4a398e2c64bd491b865a4e055873bdc7ce18720c
seemrcola <seemrcola@163.com>
2022-11-12 11:24:55 +0800

    feat: music static data

--- src/pages/music/data.ts
+++ src/pages/music/data.ts
@@ -1,5 +1,25 @@
 export default [
   {
+    "type": "qq",
+    "link": "https://y.qq.com/n/yqq/song/001Bn9v83Ozs4k.html",
+    "songid": "001Bn9v83Ozs4k",
+    "title": "红豆 (Live)",
+    "author": "王菲",
+    "lrc": "[ti:红豆 (Live)]\n[ar:王菲]\n[al:王菲幻乐一场演唱会]\n[by:]\n[offset:0]\n[00:00.00]红豆 (Live) - 王菲 (Faye Wong)\n[00:04.08]词：林夕\n[00:08.16]曲：柳重言\n[00:12.25]编曲：Alex San\n[00:16.33]还没好好地感受\n[00:19.58]\n[00:20.19]雪花绽放的气候\n[00:23.65]\n[00:24.40]我们一起颤抖\n[00:27.64]会更明白 什么是温柔\n[00:32.50]\n[00:33.08]还没跟你牵着手\n[00:36.81]\n[00:37.32]走过荒芜的沙丘\n[00:40.85]\n[00:41.54]可能从此以后 学会珍惜\n[00:46.93]天长和地久\n[00:50.20]有时候 有时候\n[00:53.89]\n[00:54.52]我会相信一切有尽头\n[00:58.11]\n[00:58.73]相聚离开 都有时候\n[01:02.58]\n[01:03.15]没有什么会永垂不朽\n[01:06.76]\n[01:07.34]可是我 有时候\n[01:10.95]\n[01:11.60]宁愿选择留恋不放手\n[01:15.31]\n[01:15.94]等到风景都看透\n[01:19.66]\n[01:20.22]也许你会陪我 看细水长流\n[01:27.23]\n[01:39.62]有时候 有时候\n[01:43.12]\n[01:43.80]我会相信一切有尽头\n[01:47.36]\n[01:48.07]相聚离开 都有时候\n[01:51.85]\n[01:52.38]没有什么会永垂不朽\n[01:56.06]\n[01:56.63]可是我 有时候\n[02:00.27]\n[02:00.92]宁愿选择留恋不放手\n[02:05.19]等到风景都看透\n[02:08.96]\n[02:09.47]也许你会陪我 看细水长流",
+    "url": "http://ws.stream.qqmusic.qq.com/C400000CUsNx0K4Jnb.m4a?guid=433908192&vkey=95F99A29E80E4EF108DB1660A1F2CBD574F97CA2318D923D3B245BC6B92185658C8CEEF0A848E9FC996B0C1C90FE4AFED6A16D89E40F9542&uin=&fromtag=120032",
+    "pic": "https://y.gtimg.cn/music/photo_new/T002R300x300M000004LmyiY0e0aGs.jpg"
+  },
+  {
+    "type": "qq",
+    "link": "https://y.qq.com/n/yqq/song/001bLkWD2EYA4P.html",
+    "songid": "001bLkWD2EYA4P",
+    "title": "天龙八部之宿敌",
+    "author": "许嵩",
+    "lrc": "[ti:天龙八部之宿敌 (《天龙八部3》网游主题曲)]\n[ar:许嵩]\n[al:天龙八部之宿敌]\n[by:]\n[offset:0]\n[00:00.00]天龙八部之宿敌 - 许嵩\n[00:05.63]词：许嵩\n[00:11.26]曲：许嵩\n[00:16.89]编曲：蔡庭贵\n[00:22.52]会在何处见到你\n[00:25.73]\n[00:27.38]莫非前尘已注定\n[00:31.26]\n[00:32.14]飞过时空的距离\n[00:37.15]却囿于刀剑光影\n[00:42.10]三月春花渐次醒\n[00:46.61]\n[00:47.14]迢迢年华谁老去\n[00:51.97]是劫是缘随我心\n[00:56.38]\n[00:56.89]除了你万敌不侵\n[01:01.94]当恩怨各一半 我怎么圈揽\n[01:07.38]看灯笼血红染 寻仇已太晚\n[01:12.10]月下门童喟叹 昨夜太平长安\n[01:21.33]\n[01:22.18]当天上星河转 我命已定盘\n[01:27.27]待绝笔墨痕干 宿敌已来犯\n[01:32.15]我借你的孤单 今生恐怕难还\n[01:41.43]\n[02:03.82]缠扰孤岛的雪雨\n[02:08.64]飘飘洒洒谁来停\n[02:13.63]摘取一颗海上星\n[02:18.03]\n[02:18.53]陪我终夜不孤寂\n[02:23.60]灵柩长埋深谷底\n[02:28.86]没有永远的秘密\n[02:32.85]\n[02:33.40]染指江湖结悲局\n[02:37.75]\n[02:38.36]无人逃得过宿命\n[02:43.42]当恩怨各一半 我怎么圈揽\n[02:48.94]看灯笼血红染 寻仇已太晚\n[02:53.91]月下门童喟叹 昨夜太平长安\n[03:02.93]\n[03:03.50]当天上星河转 我命已定盘\n[03:08.70]待绝笔墨痕干 宿敌已来犯\n[03:13.65]我借你的孤单 今生恐怕难还\n[03:22.78]\n[03:25.73]当恩怨各一半 我怎么圈揽\n[03:31.09]看灯笼血红染 寻仇已太晚\n[03:35.94]月下门童喟叹 昨夜太平长安\n[03:45.73]当天上星河转 我命已定盘\n[03:50.73]待绝笔墨痕干 宿敌已来犯\n[03:55.69]我借你的孤单 今生恐怕难还\n",
+    "url": "http://ws.stream.qqmusic.qq.com/C400002AW4wE1YtsXz.m4a?guid=133976405&vkey=93BB08231D4DBB431CEE56301427DB39DA4B96ABD07B65B1A465D218D416404BAA779DA75CAD16414B719C5DD570523F394FAA0448FA8FED&uin=&fromtag=120032",
+    "pic": "https://y.gtimg.cn/music/photo_new/T002R300x300M000002JzRQD2zjvxG.jpg"
+},
+  {
     type: 'netease',
     link: 'http://music.163.com/#/song?id=461347998',
     songid: 461347998,
@@ -8,16 +28,6 @@ export default [
     lrc: '[00:00.000] 作词 : Guy Berryman/Christopher Martin/Jonny Buckland/Andrew Taggart/Will Champion\n[00:01.000] 作曲 : Guy Berryman/Christopher Martin/Jonny Buckland/Andrew Taggart/Will Champion\n[00:02.000] 编曲 : The Chainsmokers\n[00:07.820]I\'ve been reading books of old\n[00:10.140]The legends and the myths\n[00:12.280]Achilles and his gold\n[00:14.620]Hercules and his gifts\n[00:17.100]Spiderman\'s control\n[00:19.340]And Batman with his fists\n[00:22.310]And clearly I don\'t see myself upon that list\n[00:26.070]But she said, where\'d you wanna go?\n[00:28.700]How much you wanna risk?\n[00:30.700]I\'m not looking for somebody\n[00:32.970]With some superhuman gifts\n[00:35.610]Some superhero\n[00:37.920]Some fairytale bliss\n[00:40.380]Just something I can turn to\n[00:42.600]Somebody I can kiss\n[00:44.670]I want something just like this\n[00:47.350]Doo-doo-doo, doo-doo-doo\n[00:49.560]Doo-doo-doo, doo-doo\n[00:51.880]Doo-doo-doo, doo-doo-doo\n[00:53.910]Oh I want something just like this\n[00:56.560]Doo-doo-doo, doo-doo-doo\n[00:58.960]Doo-doo-doo, doo-doo\n[01:01.240]Doo-doo-doo, doo-doo-doo\n[01:03.140]Oh I want something just like this\n[01:12.770]I want something just like this\n[01:22.460]I\'ve been reading books of old\n[01:24.630]The legends and the myths\n[01:26.880]The testaments they told\n[01:29.260]The moon and its eclipse\n[01:31.580]And Superman unrolls\n[01:33.870]A suit before he lifts\n[01:37.290]But I\'m not the kind of person that it fits\n[01:40.540]She said, where\'d you wanna go?\n[01:43.170]How much you wanna risk?\n[01:45.240]I\'m not looking for somebody\n[01:47.520]With some superhuman gifts\n[01:50.140]Some superhero\n[01:52.430]Some fairytale bliss\n[01:54.820]Just something I can turn to\n[01:57.110]Somebody I can miss\n[01:59.190]I want something just like this\n[02:08.640]I want something just like this\n[02:17.490]Oh I want something just like this\n[02:20.170]Doo-doo-doo, doo-doo-doo\n[02:22.780]Doo-doo-doo, doo-doo\n[02:25.050]Doo-doo-doo, doo-doo-doo\n[02:27.100]Oh I want something just like this\n[02:29.810]Doo-doo-doo, doo-doo-doo\n[02:32.050]Doo-doo-doo, doo-doo\n[02:34.470]Doo-doo-doo, doo-doo-doo\n[02:37.270]Where\'d you wanna go?\n[02:39.200]How much you wanna risk?\n[02:41.230]I\'m not looking for somebody\n[02:43.550]With some superhuman gifts\n[02:46.120]Some superhero\n[02:48.320]Some fairytale bliss\n[02:50.720]Just something I can turn to\n[02:53.010]Somebody I can kiss\n[02:55.150]I want something just like this\n[03:13.640]Oh I want something just like this\n[03:32.360]Oh I want something just like this\n[03:50.950]Oh I want something just like this\n',
     url: 'http://music.163.com/song/media/outer/url?id=461347998.mp3',
     pic: 'http://p4.music.126.net/7tMKza9QbB9xsiQHMmLSKw==/109951167280335904.jpg?param=300x300',
-  },
-  {
-    type: 'qq',
-    link: 'https://y.qq.com/n/yqq/song/001bLkWD2EYA4P.html',
-    songid: '001bLkWD2EYA4P',
-    title: '天龙八部之宿敌',
-    author: '许嵩',
-    lrc: '[ti:天龙八部之宿敌 (《天龙八部3》网游主题曲)]\n[ar:许嵩]\n[al:天龙八部之宿敌]\n[by:]\n[offset:0]\n[00:00.00]天龙八部之宿敌 - 许嵩\n[00:05.63]词：许嵩\n[00:11.26]曲：许嵩\n[00:16.89]编曲：蔡庭贵\n[00:22.52]会在何处见到你\n[00:25.73]\n[00:27.38]莫非前尘已注定\n[00:31.26]\n[00:32.14]飞过时空的距离\n[00:37.15]却囿于刀剑光影\n[00:42.10]三月春花渐次醒\n[00:46.61]\n[00:47.14]迢迢年华谁老去\n[00:51.97]是劫是缘随我心\n[00:56.38]\n[00:56.89]除了你万敌不侵\n[01:01.94]当恩怨各一半 我怎么圈揽\n[01:07.38]看灯笼血红染 寻仇已太晚\n[01:12.10]月下门童喟叹 昨夜太平长安\n[01:21.33]\n[01:22.18]当天上星河转 我命已定盘\n[01:27.27]待绝笔墨痕干 宿敌已来犯\n[01:32.15]我借你的孤单 今生恐怕难还\n[01:41.43]\n[02:03.82]缠扰孤岛的雪雨\n[02:08.64]飘飘洒洒谁来停\n[02:13.63]摘取一颗海上星\n[02:18.03]\n[02:18.53]陪我终夜不孤寂\n[02:23.60]灵柩长埋深谷底\n[02:28.86]没有永远的秘密\n[02:32.85]\n[02:33.40]染指江湖结悲局\n[02:37.75]\n[02:38.36]无人逃得过宿命\n[02:43.42]当恩怨各一半 我怎么圈揽\n[02:48.94]看灯笼血红染 寻仇已太晚\n[02:53.91]月下门童喟叹 昨夜太平长安\n[03:02.93]\n[03:03.50]当天上星河转 我命已定盘\n[03:08.70]待绝笔墨痕干 宿敌已来犯\n[03:13.65]我借你的孤单 今生恐怕难还\n[03:22.78]\n[03:25.73]当恩怨各一半 我怎么圈揽\n[03:31.09]看灯笼血红染 寻仇已太晚\n[03:35.94]月下门童喟叹 昨夜太平长安\n[03:45.73]当天上星河转 我命已定盘\n[03:50.73]待绝笔墨痕干 宿敌已来犯\n[03:55.69]我借你的孤单 今生恐怕难还\n',
-    url: 'http://ws.stream.qqmusic.qq.com/C400002AW4wE1YtsXz.m4a?guid=972263870&vkey=6F08D9C79A61114D632A43379433C2C86ABCAC86CA8EB1A55D05746DA065939D8EAFC90451F8112837B02B0261222BC8CC92B83C2C1192A0&uin=&fromtag=120032',
-    pic: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002JzRQD2zjvxG.jpg',
   },
   {
     type: 'netease',
@@ -30,23 +40,23 @@ export default [
     pic: 'http://p4.music.126.net/zYqptzyHld2ylpb5FOoqDA==/1785606883508227.jpg?param=300x300',
   },
   {
-    type: 'qq',
-    link: 'https://y.qq.com/n/yqq/song/001Bn9v83Ozs4k.html',
-    songid: '001Bn9v83Ozs4k',
-    title: '红豆 (Live)',
-    author: '王菲',
-    lrc: '[ti:红豆 (Live)]\n[ar:王菲]\n[al:王菲幻乐一场演唱会]\n[by:]\n[offset:0]\n[00:00.00]红豆 (Live) - 王菲 (Faye Wong)\n[00:04.08]词：林夕\n[00:08.16]曲：柳重言\n[00:12.25]编曲：Alex San\n[00:16.33]还没好好地感受\n[00:19.58]\n[00:20.19]雪花绽放的气候\n[00:23.65]\n[00:24.40]我们一起颤抖\n[00:27.64]会更明白 什么是温柔\n[00:32.50]\n[00:33.08]还没跟你牵着手\n[00:36.81]\n[00:37.32]走过荒芜的沙丘\n[00:40.85]\n[00:41.54]可能从此以后 学会珍惜\n[00:46.93]天长和地久\n[00:50.20]有时候 有时候\n[00:53.89]\n[00:54.52]我会相信一切有尽头\n[00:58.11]\n[00:58.73]相聚离开 都有时候\n[01:02.58]\n[01:03.15]没有什么会永垂不朽\n[01:06.76]\n[01:07.34]可是我 有时候\n[01:10.95]\n[01:11.60]宁愿选择留恋不放手\n[01:15.31]\n[01:15.94]等到风景都看透\n[01:19.66]\n[01:20.22]也许你会陪我 看细水长流\n[01:27.23]\n[01:39.62]有时候 有时候\n[01:43.12]\n[01:43.80]我会相信一切有尽头\n[01:47.36]\n[01:48.07]相聚离开 都有时候\n[01:51.85]\n[01:52.38]没有什么会永垂不朽\n[01:56.06]\n[01:56.63]可是我 有时候\n[02:00.27]\n[02:00.92]宁愿选择留恋不放手\n[02:05.19]等到风景都看透\n[02:08.96]\n[02:09.47]也许你会陪我 看细水长流',
-    url: 'http://ws.stream.qqmusic.qq.com/C400000CUsNx0K4Jnb.m4a?guid=216242558&vkey=376953457467DA8E8DE274A2E98AC35C1632E522ABD67BCB8A3640C383A18FCF229A114B5DF0FE4395FDF2456A09E9A24CBC91F4516C126A&uin=&fromtag=120032',
-    pic: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004LmyiY0e0aGs.jpg',
+    "type": "netease",
+    "link": "http://music.163.com/#/song?id=1674192",
+    "songid": 1674192,
+    "title": "More Than I Can Say",
+    "author": "Leo Sayer",
+    "lrc": "[00:20.000]woh -- woh -- yeah -- yeah\n[00:25.000]i love you more than i can say\n[00:29.000]i love you twice as much tomorrow\n[00:35.000]love you more than i can say\n[00:40.000]woh -- woh -- yeah -- yeah\n[00:44.000]i miss you every single day\n[00:48.000]why must my life be filled with sorrow\n[00:55.000]i love you more than i can say\n[01:00.000]Don't you know i need you so\n[01:05.000]oh tell me please i gotta know\n[01:10.400]do you mean to make me cry\n[01:15.100]am i just another guy\n[01:19.100]woh -- woh -- yeah -- yeah\n[01:24.100]i miss you more than i can say\n[01:29.100]why must my life be filled with sorrow\n[01:34.100]i love you more than i can say\n[02:00.100]oh dont you know i need you so\n[02:04.100]oh tell me please i gotta know\n[02:10.100]do you mean to make me cry\n[02:14.100]am i just another guy\n[02:19.100]woh -- woh -- yeah -- yeah\n[02:23.100]i love you more than i can say\n[02:29.100]i love you twice as much tomorrow\n[02:34.100]love you more than i can say\n[02:39.100]i love you more than i can say\n[02:44.100]i love you more than i can say\n[02:49.100]woh more than i can say\n[03:00.100]more than i can say\n[03:02.100]i love you twice as much tomorrow\n[03:10.100]more than i can say\n[03:13.200]i love you more than i can say\n[03:22.100]more than i can say\n[03:23.800]i love you more than i can say\n",
+    "url": "http://music.163.com/song/media/outer/url?id=1674192.mp3",
+    "pic": "http://p4.music.126.net/vrm4uUMofhUNQH3pGg5khQ==/109951164931750033.jpg?param=300x300"
   },
   {
-    type: 'netease',
-    link: 'http://music.163.com/#/song?id=233956',
-    songid: 233956,
-    title: 'You Raise Me Up',
-    author: 'G.E.M.邓紫棋',
-    lrc: '[00:39.440] when I am down and, oh my soul, so weary;\n[00:48.240] When troubles come and my heart burdened be;\n[00:55.540] Then, I am still and wait here in the silence,\n[01:05.040] Until you come and sit awhile with me.\n[01:12.640] You raise me up, so I can stand on mountains;\n[01:22.040] You raise me up, to walk on stormy sea;\n[01:30.540] I am strong, when I am on your shoulders;\n[01:38.540] You raise me up... To more than I can be.\n[02:21.040] You raise me up, so I can stand on mountains;\n[02:29.640] You raise me up, to walk on stormy seas;\n[02:38.640] I am strong, when I am on your shoulders;\n[02:54.340] You raise me up... To more than I can be.\n',
-    url: 'http://music.163.com/song/media/outer/url?id=233956.mp3',
-    pic: 'http://p3.music.126.net/btmsQGuCwT6YNbbaOwGqxw==/109951165628217052.jpg?param=300x300',
-  },
+    "type": "netease",
+    "link": "http://music.163.com/#/song?id=1924380496",
+    "songid": 1924380496,
+    "title": "杀死比尔 野兽之战Battle Without Honor Or Humanity",
+    "author": "崔晨煜",
+    "lrc": "",
+    "url": "http://music.163.com/song/media/outer/url?id=1924380496.mp3",
+    "pic": "http://p3.music.126.net/ttZOgDgW0MMNMCJxSAw63w==/109951167103368181.jpg?param=300x300"
+  }
 ]
